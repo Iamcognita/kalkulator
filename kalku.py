ffbf3f5f80f0e5d36b547fa72211c365c9481658
Iamcognita <84830013+Iamcognita@users.noreply.github.com>
2021-05-26 11:58:15 +0700

    Add files via upload

---
+++ kalku.py
@@ -0,0 +1,25 @@
+#python 3.7.3
+ 
+print("ini adalah program kalkulator sederhana by Cognita") 
+x = input('masukkan angka ke 1 :')
+y = input('masukkan angka ke 2 :')
+num1 = int(x)
+num2 = int(y)
+print(num1, '+', num2, '=', num1 + num2)
+
+
+
+x = input('masukkan angka ke 1 :')
+y = input('masukkan angka ke 2 :')
+num1 = int(x)
+num2 = int(y)
+print(num1, '-', num2, '=', num1 -  num2)
+
+
+
+x = input('masukkan angka ke 1 :')
+y = input('masukkan angka ke 2 :')
+num1 = int(x)
+num2 = int(y)
+print(num1, '*', num2, '=', num1 *  num2)
+
